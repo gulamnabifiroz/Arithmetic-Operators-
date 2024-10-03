06b52aebefbddf52fa970fc0e6147877deb39db3
gulamnabifiroz <khangulam2005@gmail.com>
2024-10-03 11:46:11 +0530

    Add files via upload

---
+++ Sum Of Two Numbers.cpp
@@ -0,0 +1,23 @@
+// Name - Gulamnabi Khan
+// PRN - 23070123503
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1;
+    int number2;
+    int sum;
+    cout << "Enter the first number: ";
+    cin >> number1;
+    cout << "Enter the second number: ";
+    cin >> number2;
+    sum = number1 + number2;
+    cout << "The sum of " << number1 << " and " << number2 << " is: " << sum << endl;
+    return 0;
+}
+/*Output
+Enter the first number: 10
+Enter the second number: 20
+The sum of 10 and 20 is: 30
+*/
